017aa5ee108b1d113678e8d853017a355339362e
Nikunj-Gupta <Nikunj-Gupta@github.com>
2019-12-15 14:03:06 +0530

    minor updates

--- cnn.py
+++ cnn.py
@@ -8,25 +8,24 @@ import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 
-from tensorflow.keras import layers, models
+from keras import layers, models
 
-EPOCHS = 500
+EPOCHS = 5
 # TRAIN_SPLIT = 200
 TEST_SPLIT = 4000
 
 
-def get_labels(annotations, size):
-	labels = np.zeros((size, 5))
+def get_labels(annotations_temp, size):
+	labels_temp = np.zeros((size, 5))
 	for i in range(size):
 		for j in range(5):
-			labels[i, j] = annotations['annotations'][0][i][j][0][0]
-	labels = pd.DataFrame(labels, columns=('bbox_x1', 'bbox_x2', 'bbox_y1', 'bbox_y2', 'class'))
-	return labels
+			labels_temp[i, j] = annotations_temp['annotations'][0][i][j][0][0]
+	labels_temp = pd.DataFrame(labels_temp, columns=('bbox_x1', 'bbox_x2', 'bbox_y1', 'bbox_y2', 'class'))
+	return labels_temp
 
 
-def peek_image(path, index, labels):
+def peek_image(path, index, labels_temp):
 	path = '../stanford_data/' + path + '/'
-	image_names = os.listdir(path)
 	name = str(index)
 	for i in range(5 - len(name)):
 		name = '0' + name
@@ -35,11 +34,11 @@ def peek_image(path, index, labels):
 	h_resize = int(128 * 1.5)
 	w_resize = 128
 	im = cv2.resize(im, (h_resize, w_resize), interpolation=cv2.INTER_LINEAR)
-	print('The label for this car is: ', labels['class'][index - 1])
+	print('The label for this car is: ', labels_temp['class'][index - 1])
 	plt.imshow(im)
 
 
-def read_image(path, labels, test=False):
+def read_image(path, labels_temp, test=False):
 	path = '../stanford_data/' + path + '/'
 	image_names = os.listdir(path)
 	x = []
@@ -57,45 +56,48 @@ def read_image(path, labels, test=False):
 		im = cv2.resize(im, (h_resize, w_resize), interpolation=cv2.INTER_LINEAR)
 		x.append(im)
 		index = int(file.split('.')[0])
-		y.append(labels['class'][index - 1])
+		y.append(labels_temp['class'][index - 1])
 	return np.array(x), np.array(y)
 
 
-def create_model(input_shape, output_shape):
-	model = models.Sequential()
-	model.add(layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=input_shape))
-	model.add(layers.MaxPool2D(pool_size=(2, 2)))
-	model.add(layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
-	model.add(layers.MaxPool2D(pool_size=(2, 2)))
-	model.add(layers.Conv2D(filters=64, kernel_size=3, activation='relu'))
-	model.add(layers.Flatten())
-	model.add(layers.Dense(64, activation='relu'))
-	model.add(layers.Dense(output_shape, activation='softmax'))
-	return model
+def create_model(input_shape_temp, output_shape_temp):
+	model_temp = models.Sequential()
+	model_temp.add(layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=input_shape_temp))
+	model_temp.add(layers.MaxPool2D(pool_size=(2, 2)))
+	model_temp.add(layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+	model_temp.add(layers.MaxPool2D(pool_size=(2, 2)))
+	model_temp.add(layers.Conv2D(filters=64, kernel_size=3, activation='relu'))
+	model_temp.add(layers.Flatten())
+	model_temp.add(layers.Dense(64, activation='relu'))
+	model_temp.add(layers.Dense(output_shape_temp, activation='softmax'))
+	return model_temp
 
 
-def train_model(model, x_train, y_train, x_test, y_test):
-	model.compile(optimizer='adam',
-				  loss='sparse_categorical_crossentropy',
-				  metrics=['accuracy'])
+def train_model(model_temp, x_train_temp, y_train_temp, x_test_temp, y_test_temp):
+	model_temp.compile(optimizer='adam',
+					   loss='sparse_categorical_crossentropy',
+					   metrics=['accuracy'])
 
-	history = model.fit(x_train, y_train,
-						epochs=EPOCHS,
-						validation_data=(x_test, y_test))
-	return history
+	history_temp = model_temp.fit(x_train_temp, y_train_temp,
+								  epochs=EPOCHS,
+								  validation_data=(x_test_temp, y_test_temp))
+	return history_temp
 
 
-def eval_model(model, history, x_test, y_test):
-	plt.plot(history.history['acc'], label='accuracy')
-	plt.plot(history.history['val_acc'], label='val_accuracy')
+def eval_model(model_temp, history_temp, x_test_temp, y_test_temp):
+	plt.plot(history_temp.history['acc'], label='accuracy')
+	plt.plot(history_temp.history['val_acc'], label='val_accuracy')
 	plt.xlabel('Epoch')
 	plt.ylabel('Accuracy')
 	plt.ylim([0.5, 1])
 	plt.legend(loc='lower right')
 
-	test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+	test_loss, test_acc = model_temp.evaluate(x_test_temp, y_test_temp, verbose=2)
 	print("Test Loss = ", test_loss)
 	print("Test Accuracy = ", test_acc)
+
+def resnet_model(inp_shape, out_shape):
+	return
 
 
 def gap():
@@ -103,6 +105,7 @@ def gap():
 	print()
 	print()
 	print()
+
 
 annotations = sio.loadmat('../stanford_data/devkit/cars_train_annos.mat')
 _, train_size = annotations['annotations'].shape
